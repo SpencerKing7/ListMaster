566371c6072508dd60fa981306f9d359a4357d88
SpencerKing7 <spencerking.c@gmail.com>
2026-04-01 12:48:49 -0500

    build: deploy 2026-04-01 12:48:49

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,5 +1,4 @@
 // src/components/HeaderBar.tsx
-import { Button } from "@/components/ui/button";
 import { useSettingsStore } from "@/store/useSettingsStore";
 import CategoryPicker from "./CategoryPicker";
 
@@ -56,11 +55,10 @@ const HeaderBar = ({ onOpenSettings, scrolled = false, onRefresh }: HeaderBarPro
             <path d="M8 16H3v5" />
           </svg>
         </button>
-        <Button
-          variant="ghost"
-          size="icon"
+        <button
           onClick={onOpenSettings}
-          className="shrink-0"
+          className="relative shrink-0 w-10 h-10 rounded-full flex items-center justify-center transition-transform duration-150 active:scale-[0.96]"
+          style={{ touchAction: "manipulation" }}
         >
           <svg
             width="22"
@@ -71,7 +69,7 @@ const HeaderBar = ({ onOpenSettings, scrolled = false, onRefresh }: HeaderBarPro
           >
             <path d="M19.14 12.94c.04-.31.06-.63.06-.94 0-.31-.02-.63-.06-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.49.49 0 0 0-.59-.22l-2.39.96a7.04 7.04 0 0 0-1.62-.94l-.36-2.54a.48.48 0 0 0-.48-.41h-3.84a.48.48 0 0 0-.48.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96a.49.49 0 0 0-.59.22L2.74 8.87a.48.48 0 0 0 .12.61l2.03 1.58c-.04.31-.06.63-.06.94s.02.63.06.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.26.41.48.41h3.84c.24 0 .44-.17.48-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6A3.6 3.6 0 1 1 12 8.4a3.6 3.6 0 0 1 0 7.2z" />
           </svg>
-        </Button>
+        </button>
       </div>
       <CategoryPicker />
     </header>
