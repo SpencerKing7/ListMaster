a1aaead50a04d05738bf14721f1d91e3a9889399
SpencerKing7 <spencerking.c@gmail.com>
2026-04-03 17:43:35 -0500

    build: deploy 2026-04-03 17:43:35

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -91,7 +91,7 @@ function DialogContent({
       <DialogPrimitive.Popup
         data-slot="dialog-content"
         className={cn(
-          "fixed left-1/2 z-[70] grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-2xl p-4 text-sm duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
+          "fixed left-1/2 z-[71] grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-2xl p-4 text-sm duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
           className
         )}
         style={{
